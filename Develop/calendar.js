d4bfd443b3778c260aac75b9203fcb97ff2e9c39
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-03 20:32:12 -0500

    setup hours

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -2,13 +2,13 @@
 let d = new Date()
 
 let year = d.getFullYear()
-let mohth = d. getMonth () + 1;
+let month = d. getMonth () + 1;
 let day = d.getDate()
 
 let totalDays = new Date (year, month, 0).getDate()
 
 let output =
-(mohth<10 ? '0' : '') + month + '/'+
+(month<10 ? '0' : '') + month + '/'+
 (day<10 ? '0' : '') + day + '/' + year
 
-$('.date').text('${output}')
+$('.date').text(`${output}`)
