02736ed1888fa4c8f79e79e7b29ccd20403bc331
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-03 21:23:58 -0500

    setup local storage and save buttons

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -1,14 +1,39 @@
 
-let d = new Date()
+function update(){
+    var hour=moment().hours()
+    $(".time-block").each(function(){
+        var blockHour = parseInt($(this).attr("id").split("-")[1])
+        if(blockHour < hour){
+            $(this).addClass("past")
+        }
+        else if (blockHour ===hour){
+            $(this).removeClass("past")
+            $(this).addClass("present") 
+        }
+        else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+    })
+}
 
-let year = d.getFullYear()
-let month = d. getMonth () + 1;
-let day = d.getDate()
+$(".saveBtn").on("click", function(){
+    var value = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, value)
+})
 
-let totalDays = new Date (year, month, 0).getDate()
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
-let output =
-(month<10 ? '0' : '') + month + '/'+
-(day<10 ? '0' : '') + day + '/' + year
-
-$('.date').text(`${output}`)
+$(".date").text(moment().format("dddd, MMMM Do"))
+update()
+var interval = setInterval(update, 15000)
